516ac1840f5de5bd6f6cf65453bcf4d744ce17ab
Dwight Thompson <dthomp95@stanford.edu>
2026-05-27 17:57:41 -0700

    first initial scaffold

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # CS151ProjectWebsite
 Group Project for CS151 Website Implementing Elevator Problem
+
+---
+
+
+# Tasks Completed
+- Finished importing epilog.js (in epilog scripts)
+- Added scaffolding to website design
+- Added sliders for main design
+- Added basic assets to assets page
+
+# Tasks To-Do
+- Set up animation process
+- Create logic program with basic variables
+- Format settings towards bottom
+- Think about making tests
---
+++ script.js
@@ -0,0 +1,27 @@
+function init() {
+    // initializes the program, runs once page loads.
+    setValue();
+}
+
+document.addEventListener("DOMContentLoaded", init);
+
+function resetProgram() {
+    // everything that they've done so far should reset here.
+    // all variables, all animations, outputs should halt & reset
+
+
+}
+
+
+function setValue() {
+    /* This function handles the paragraphs that change with user sliders. */
+    resetProgram();
+
+    var num = document.querySelector("#elevator-number-input").value.toString();
+    document.querySelector("#elevator-num").textContent = num;
+
+    var speed = document.querySelector("#elevator-speed-input").value.toString();
+    document.querySelector("#elevator-speed").textContent = speed;
+}
+
+
